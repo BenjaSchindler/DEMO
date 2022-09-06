4b3d1d29ecdd444daabf73528c522502b83aebd7
Benjamin Schindler <benja.schindler2@gmail.com>
2022-09-05 22:31:37 -0300

    Primer avance

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home';
+import Nav from './Components/nav';
+import Blog from './Routes/Blog';
 
 
 const App = () => {
   return (
     <>
-      
+      <Nav/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="Home" element={<Home />} />
+        <Route path="Blog" element={<Blog />} />
       </Routes>
     </>
   );
